0101dbb86408f408d9d17c689a156334dd5b20dc
Cody Daniels <codydaniels@gmail.com>
2015-12-30 18:05:18 -0700

    Add parsing api

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 var express = require('express');
 var favicon = require('serve-favicon');
 var path = require('path');
+
+// Initialize Express App
 var app = express();
 
 // Middleware
@@ -13,7 +15,34 @@ app.get('/', function(req, res) {
 });
 
 app.get('/:date', function(req, res) {
-  
+  var date;
+  var options = {
+    timeZone: 'UTC',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
+  };
+
+  if(isNaN(req.params.date)) {
+    date = new Date(req.params.date + " UTC");
+    if(date == "Invalid Date") {
+      res.json({
+        unix: null,
+        natural: null
+      });
+    } else {
+      res.json({
+        unix: date.getTime() / 1000,
+        natural: date.toLocaleDateString('en-US', options)
+      });
+    }
+  } else {
+    date = new Date(+req.params.date*1000);
+    res.json({
+      unix: +req.params.date,
+      natural: date.toLocaleDateString('en-US', options)
+    });
+  }
 });
 
 
